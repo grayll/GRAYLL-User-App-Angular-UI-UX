ee3af632d7a6d711d7fac12afe7375d3708a5985
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-07-31 21:31:32 +0200

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from './authorization/login/login.component';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: LoginComponent
-  }
 ];
 
 @NgModule({
