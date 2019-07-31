ecc28696beff77a09d21b945b705c2dca278f8df
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-07-31 22:52:15 +0200

    Style changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './authorization/login/login.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {faKey, faUser} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,4 @@ import {faKey, faUser} from '@fortawesome/free-solid-svg-icons';
 })
 export class AppComponent {
   title = 'code';
-  userIcon = faUser;
-  keyIcon  = faKey;
 }
