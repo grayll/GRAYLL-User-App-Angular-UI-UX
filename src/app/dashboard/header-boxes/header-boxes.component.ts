38f2727e144172a58383fee0a81eab5a62808049
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-08-01 17:01:45 +0200

    Remove unecessary code

--- src/app/dashboard/header-boxes/header-boxes.component.ts
+++ src/app/dashboard/header-boxes/header-boxes.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header-boxes',
   templateUrl: './header-boxes.component.html',
   styleUrls: ['./header-boxes.component.css']
 })
-export class HeaderBoxesComponent implements OnInit {
+export class HeaderBoxesComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
