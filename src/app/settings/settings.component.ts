e3e8538bb80a44cfe3497df0c29b88dc77f6f3b7
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-08-07 11:19:09 +0200

    Made security page responsive for mobile added tabset

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faBell, faChartBar, faCommentAlt, faLock, faUser, faWallet} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-settings',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  
+  faUser = faUser;
+  faBell = faBell;
+  faComment = faCommentAlt;
+  faChartBar = faChartBar;
+  faWallet = faWallet;
+  faLock = faLock;
 
   constructor() { }
 
