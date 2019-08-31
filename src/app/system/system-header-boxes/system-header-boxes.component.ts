238afe99868516ebb77dd07fa80419929a9ed423
nedim1511 <nedim.kurbegovic@stu.ssst.edu.ba>
2019-08-31 22:08:18 +0200

    Improve styling and perform updates

--- src/app/system/system-header-boxes/system-header-boxes.component.ts
+++ src/app/system/system-header-boxes/system-header-boxes.component.ts
@@ -10,18 +10,46 @@ import {CountdownConfig} from 'ngx-countdown/src/countdown.config';
 export class SystemHeaderBoxesComponent implements OnInit {
 
   faInfo = faInfoCircle;
+  GRXValue: string;
+  totalGRX: number;
+  selectedTab: any;
   countdownConfig: CountdownConfig = {
     leftTime: 60,
     template: '$!s!',
     effect: null,
     demand: false
   };
-  GRXValue: string;
-  totalGRX: number;
+  algoItems = [
+    {
+      id: 'GRY 1',
+      name: 'GRY | 1',
+      value: 'Balthazar',
+      tabName: 'Balthzr'
+    },
+    {
+      id: 'GRY 2',
+      name: 'GRY | 2',
+      value: 'Kaspar',
+      tabName: 'Kaspar'
+    },
+    {
+      id: 'GRY 3',
+      name: 'GRY | 3',
+      value: 'Melkior',
+      tabName: 'Melkior'
+    },
+    {
+      id: 'GRZ',
+      name: 'GRZ',
+      value: 'Arkady',
+      tabName: 'Arkady'
+    }
+  ];
 
   constructor() {
     this.GRXValue = null;
     this.totalGRX = 99999999999.99998;
+    this.selectedTab = this.algoItems[0];
   }
 
   ngOnInit() {
@@ -29,6 +57,10 @@ export class SystemHeaderBoxesComponent implements OnInit {
 
   populateMaxGRX() {
     this.GRXValue = this.totalGRX.toString();
+  }
+
+  didChangeTab(id: string) {
+    this.selectedTab = this.algoItems.find((i) => i.id === id);
   }
 
 }
