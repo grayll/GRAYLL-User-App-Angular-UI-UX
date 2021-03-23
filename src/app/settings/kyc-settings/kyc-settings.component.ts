3da8b65d244aaf84a696f6dbfa7603aa6f3023ac
newhopestar1002 <newhopestar1002@gmail.com>
2021-03-23 15:23:17 +0100

    User KYC page tick box updates

--- src/app/settings/kyc-settings/kyc-settings.component.ts
+++ src/app/settings/kyc-settings/kyc-settings.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { SnotifyService } from 'ng-snotify';
-import {faCalendar} from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-kyc-settings',
@@ -13,21 +13,21 @@ import {Router} from '@angular/router';
 export class KycSettingsComponent implements OnInit {
 
   userData = {
-    firstName:'',
-    lastName:'',
-    birthday:'',
-    nationality:'',
-    governmentID:'',
-    personalORcompany:''
+    firstName: '',
+    lastName: '',
+    birthday: '',
+    nationality: '',
+    governmentID: '',
+    personalORcompany: ''
   }
 
   companyData = {
-    companyName:'',
-    companyRegistration:'',
-    addressLine1:'',
-    addressLine2:'',
-    city:'',
-    country:''
+    companyName: '',
+    companyRegistration: '',
+    addressLine1: '',
+    addressLine2: '',
+    city: '',
+    country: ''
   }
 
   model: NgbDateStruct;
@@ -40,11 +40,14 @@ export class KycSettingsComponent implements OnInit {
   driverLicense = "";
 
   government_id_select = "";
-  income_select ="";
+  income_select = "";
   address_select = "";
   personal_liquid_assets_select = "";
   company_registration_select = "";
-  company_financials_select = ""; 
+  company_financials_select = "";
+  isFileUploaded = false;
+
+  @ViewChild('fileUploader') fileUploader: ElementRef;
 
   constructor(private snotifyService: SnotifyService, private router: Router) { }
 
@@ -52,22 +55,9 @@ export class KycSettingsComponent implements OnInit {
   }
 
   handleFileInput(files: FileList) {
-    if( this.government_id_select || this.income_select || this.address_select || this.personal_liquid_assets_select 
-       || this.personal_liquid_assets_select || this.company_registration_select || this.company_financials_select  ) {
       this.fileToUpload = files.item(0);
       console.log(files);
-      this.router.navigate(['/settings/profile', {outlets: {popup: 'kyc-document-upload-in-progress'}}])
-      this.government_id_select = "";
-      this.income_select = "";
-      this.address_select = "";
-      this.personal_liquid_assets_select = "" ;
-      this.company_registration_select = "";
-      this.company_financials_select = "";
-    } else {
-      //this.router.navigate(['/settings/profile', {outlets: {popup: 'kyc-document-upload-unsuccessful'}}])
-      this.snotifyService.simple('You should select one checkbox')
-    }
-    
+      this.router.navigate(['/settings/profile', { outlets: { popup: 'kyc-document-upload-in-progress' } }])
   }
 
   usersave() {
@@ -78,40 +68,34 @@ export class KycSettingsComponent implements OnInit {
     this.snotifyService.simple('Your company data changes are saved.');
   }
 
-  idCheck(event){
-    if (event) {
-      this.government_id_select = event.target.value;
-      console.log("event:", event.target.value)
-    }
+  idCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.government_id_select = event.target.value;
+    console.log("event:", event.target.value)
   }
 
-  incomeCheck(event){
-    if (event) {
-      this.income_select = event.target.value;
-    }
+  incomeCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.income_select = event.target.value;
   }
 
-  addressCheck(event){
-    if (event) {
-      this.address_select = event.target.value;
-    }
+  addressCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.address_select = event.target.value;
   }
 
-  personalLiquidAssetsCheck(event){
-    if (event) {
-      this.personal_liquid_assets_select = event.target.value;
-    }
+  personalLiquidAssetsCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.personal_liquid_assets_select = event.target.value;
   }
 
-  companyRegistrationCheck(event){
-    if (event) {
-      this.company_registration_select = event.target.value;
-    }
+  companyRegistrationCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.company_registration_select = event.target.value;
   }
 
-  companyFinancialsCheck(event){
-    if (event) {
-      this.company_financials_select = event.target.value;
-    }
+  companyFinancialsCheck(event) {
+    this.fileUploader.nativeElement.click();
+    this.company_financials_select = event.target.value;
   }
 }
